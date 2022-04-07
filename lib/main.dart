51372e4ef74730c10a18fa4c11992eafe839ada9
dilberhusainkhan <dilberjmi@gmail.com>
2022-04-08 02:06:52 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   double pi = 3.14;
+
   bool isMale = true;
-  num temp = 30.5; //store both int or double
-  var koiBhiValue = "Koi bhi"; // we can store any value
-  final gravity = 9.8; // for const we use const or final :-we can perform addition in list in final but not in const
 
+  num temp = 30.5;
+  //store both int or double
+  var koiBhiValue = "Koi bhi";
+  // we can store any value
+  final gravity = 9.8;
 
+  // for const we use const or final :-we can perform addition in list in final but not in const
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage()
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeData(primarySwatch: Colors.red),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
